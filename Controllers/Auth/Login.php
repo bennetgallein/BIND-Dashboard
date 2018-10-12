030cb83a8642afd5d435e4833f269a0ba93c6c7d
bennetgallein <bennet@intranetproject.net>
2018-10-13 00:45:49 +0200

    added basic login functionality.

---
+++ Controllers/Auth/Login.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bennet
+ * Date: 12.10.18
+ * Time: 23:14
+ */
+
+namespace Controllers\Auth;
+
+
+use Angle\Engine\Template\Engine;
+use Controllers\Dashboard;
+use Objects\User;
+
+class Login {
+
+    public static function render(Engine $engine) {
+        $engine->render("views/index.html", array());
+    }
+
+    public static function login() {
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        $result = Dashboard::getDatabase()->fetchRow("SELECT * FROM users WHERE email = :email", ['email' => $email]);
+
+        if ($result) {
+            if (password_verify($password, $result['password'])) {
+                $user = new User($result['id'], $result['uname'], $result['email']);
+                echo json_encode(["success" => "true"]);
+            } else {
+                echo json_encode(["success" => "false", "error" => "Credentials are wrong!"]);
+            }
+        } else {
+            echo json_encode(["success" => "false", "error" => "User not found!"]);
+        }
+    }
+}
--- index.php
+++ index.php
@@ -3,6 +3,8 @@ use Tracy\Debugger;
 
 require("vendor/autoload.php");
 
+session_start();
+
 Debugger::enable(Debugger::DETECT);
 
 define("APP_URL", "BIND-Dashboard");
